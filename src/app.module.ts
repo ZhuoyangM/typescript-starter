84c9d68f5bdb7256d184d2a02e2bb2d3916cc6d6
ZhuoyangM <zhmeng@ucsd.edu>
2024-03-24 16:23:56 -0700

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/user.entity';
+import { Event } from './event/event.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 5432,
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      entities: [User, Event],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
